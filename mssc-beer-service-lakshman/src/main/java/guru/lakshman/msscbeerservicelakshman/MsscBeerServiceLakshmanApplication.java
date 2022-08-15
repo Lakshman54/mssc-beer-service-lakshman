012294ca2c477c499ac434ca34a331ec3d0e7b4c
lakshman E <Lakshman52@gmail.com>
2022-08-15 18:53:17 +0530

    initial commit.

---
+++ mssc-beer-service-lakshman/src/main/java/guru/lakshman/msscbeerservicelakshman/MsscBeerServiceLakshmanApplication.java
@@ -0,0 +1,13 @@
+package guru.lakshman.msscbeerservicelakshman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsscBeerServiceLakshmanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsscBeerServiceLakshmanApplication.class, args);
+	}
+
+}
---
+++ mssc-beer-service-lakshman/src/test/java/guru/lakshman/msscbeerservicelakshman/MsscBeerServiceLakshmanApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.lakshman.msscbeerservicelakshman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBeerServiceLakshmanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
